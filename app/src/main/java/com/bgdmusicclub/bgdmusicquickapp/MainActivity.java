67f44a6c0622920a87369e9f04f307e5030c7d2a
cameramanbrayton <info@duelingworlds.com>
2021-07-20 02:57:41 -0500

    001 BGD MUSIC Quick App and43_New Layout_Latest-Release-3.1--add Music Apps002

--- app/src/main/java/com/bgdmusicclub/bgdmusicquickapp/MainActivity.java
+++ app/src/main/java/com/bgdmusicclub/bgdmusicquickapp/MainActivity.java
@@ -19,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.musicAppBgdLinkText);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
 
+        textView = findViewById(R.id.musicAppRcbLinkText);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
         textView = findViewById(R.id.homerBgdLinkText);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
 
